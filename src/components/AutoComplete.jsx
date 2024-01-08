115570c1991198cfba7392e7d7886499890aff25
Reece Russell <rerussell@protonmail.com>
2024-01-08 13:30:51 +0200

    Modfied - Temp Comment out functionality

--- src/components/AutoComplete.jsx
+++ src/components/AutoComplete.jsx
@@ -8,46 +8,50 @@ const SearchBox = ({ onSearch }) => {
     const [searchTerm, setSearchTerm] = useState('');
     const [suggestions, setSuggestions] = useState([]);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                // Rebuild, place holder currently
-                const response = await fetch("http://127.0.0.1:8000/companies");
-                const data = await response.json();
-                setApiData(data.data);
-            } catch (error) {
-                console.error("Error fetching data:", error);
-            }
-        };
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //         try {
+    //             // Rebuild, place holder currently
+    //             const response = await fetch("http://127.0.0.1:8000/companies");
+    //             const data = await response.json();
+    //             setApiData(data.data);
+    //         } catch (error) {
+    //             console.error("Error fetching data:", error);
+    //         }
+    //     };
 
-        fetchData();
-    }, []);
+    //     fetchData();
+    // }, []);
 
-    const handleSearchChange = (event) => {
-        const newSearchTerm = event.target.value;
-        setSearchTerm(newSearchTerm);
+    // const handleSearchChange = (event) => {
+    //     const newSearchTerm = event.target.value;
+    //     setSearchTerm(newSearchTerm);
 
-        // Filter suggestions based on notices, SIC codes, and company names
-        if (apiData) {
-            const matchingSuggestions = Object.values(apiData).reduce((acc, company) => {
-                const notices = Object.values(company.notices || {});
-                const sicCodes = company.sic_codes || [];
-                const companyName = company.company_title || "";
+    //     // Filter suggestions based on notices, SIC codes, and company names
+    //     if (apiData) {
+    //         const matchingSuggestions = Object.values(apiData).reduce((acc, company) => {
+    //             const notices = Object.values(company.notices || {});
+    //             const sicCodes = company.sic_codes || [];
+    //             const companyName = company.company_title || "";
 
-                if (
-                    notices.some((notice) => notice.published_date.includes(newSearchTerm)) ||
-                    sicCodes.some((sicCode) => sicCode.includes(newSearchTerm)) ||
-                    companyName.toLowerCase().includes(newSearchTerm.toLowerCase())
-                ) {
-                    acc.push(companyName);
-                }
+    //             if (
+    //                 notices.some((notice) => notice.published_date.includes(newSearchTerm)) ||
+    //                 sicCodes.some((sicCode) => sicCode.includes(newSearchTerm)) ||
+    //                 companyName.toLowerCase().includes(newSearchTerm.toLowerCase())
+    //             ) {
+    //                 acc.push(companyName);
+    //             }
 
-                return acc;
-            }, []);
+    //             return acc;
+    //         }, []);
 
-            setSuggestions(matchingSuggestions);
-        }
-    };
+    //         setSuggestions(matchingSuggestions);
+    //     }
+    // };
+
+    const handleSearchChange = () => {
+        console.log("incomplete functionality")
+    }
 
     const handleSuggestionClick = (suggestion) => {
         setSearchTerm(suggestion);
@@ -73,7 +77,7 @@ const SearchBox = ({ onSearch }) => {
                 bg={colorMode === 'light' ? 'white' : 'blue.900'}
                 onChange={handleSearchChange}
             />
-            {suggestions.length > 0 && (
+            {/* {suggestions.length > 0 && (
                 <Box position='absolute' width='100%'>
                     <List mt='2' bg='white' borderRadius='md' boxShadow='md'>
                         {suggestions.map((suggestion, index) => (
@@ -83,7 +87,7 @@ const SearchBox = ({ onSearch }) => {
                         ))}
                     </List>
                 </Box>
-            )}
+            )} */}
         </InputGroup>
     );
 };
