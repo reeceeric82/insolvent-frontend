959a01fd72ce05c2dde7e4ce81a2502133c1775c
Reece Russell <rerussell@protonmail.com>
2024-02-08 23:22:14 +0200

    Modified - Added in session state handler

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,16 @@
 import '@/styles/globals.css'
+import { SessionProvider } from 'next-auth/react';
 import { ChakraProvider, CSSReset, ColorModeScript } from '@chakra-ui/react'
 import theme from '@/components/theme'
 
 export default function App({ Component, pageProps }) {
   return (
-    <ChakraProvider theme={theme}>
-      <CSSReset />
-      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <SessionProvider session={pageProps.session}>
+      <ChakraProvider theme={theme}>
+        <CSSReset />
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </SessionProvider>
   )
 }
